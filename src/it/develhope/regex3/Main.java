36ce70ad39247214efcfcca21ea6a97bb0d807f2
Lorenzo Balestrini <lo.bale@libero.it>
2023-01-03 14:29:43 +0100

    Regex 3 - correzione

--- src/it/develhope/regex3/Main.java
+++ src/it/develhope/regex3/Main.java
@@ -3,7 +3,7 @@ package it.develhope.regex3;
 public class Main {
     public static void main(String[] args) {
 
-        String are = "are you able to climb, are you able to swim or are you able to fly?";
-        System.out.println(are.replaceAll("\s+are","_xyz"));
+        String are = " are you able to climb, are you able to swim or are you able to fly?";
+        System.out.println(are.replaceAll(" are","_xyz"));
     }
 }
